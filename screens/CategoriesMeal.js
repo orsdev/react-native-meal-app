fa50da4cdbf0c8a45e0c6635bc8928857020731d
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-08-28 08:59:09 +0100

    navigation route option removed

--- screens/CategoriesMeal.js
+++ screens/CategoriesMeal.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { View, StyleSheet, Text, Button } from 'react-native';
 
+import { CATEGORIES } from '../data/dummy-data';
+
 const CategoriesMeal = ({ route, navigation }) => {
+   const { categoryId } = route.params;
+   const findCategoryById = CATEGORIES.find(({ id }) => categoryId === id);
+
+   React.useEffect(() => {
+      if (findCategoryById) {
+         navigation.setOptions({ title: findCategoryById.title });
+      }
+   }, [findCategoryById]);
+
+
+   if (!findCategoryById) {
+      return (
+         <View style={styles.container}>
+            <Text style={styles.title}> Meal not found</Text>
+            <Button
+               title="Go to details"
+               onPress={() => navigation.navigate('Details')} />
+         </View>
+      )
+   }
+
    return (
       <View style={styles.container}>
          <Text style={styles.title}> Categories Meal Screen</Text>
+         <Text style={styles.title}>{findCategoryById.title}</Text>
          <Button
             title="Go to details"
             onPress={() => navigation.navigate('Details')} />
