cf4b3d1c60f423166289e24c9239f2e6e15b9c0c
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-08-29 17:27:41 +0100

    ✨OverflowMenuProvider wrapped arround child component

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { OverflowMenuProvider } from 'react-navigation-header-buttons';
 
 import Colors from '../constants/Colors';
 
@@ -13,29 +14,31 @@ const Stack = createNativeStackNavigator();
 function MealsNavigation() {
    return (
       <NavigationContainer>
-         <Stack.Navigator
-            screenOptions={{
-               headerStyle: {
-                  backgroundColor: Colors.primary,
-               },
-               headerTintColor: Colors.text,
-               headerTitleStyle: {
-                  fontWeight: 'bold',
-               },
-            }}>
-            <Stack.Screen
-               name="Categories"
-               component={CategoriesScreen}
-            />
-            <Stack.Screen
-               name="Meals"
-               component={CategoriesMealScreen}
-               options={{
-                  title: 'Meals'
-               }}
-            />
-            <Stack.Screen name="Details" component={MealDetailScreen} />
-         </Stack.Navigator>
+         <OverflowMenuProvider>
+            <Stack.Navigator
+               screenOptions={{
+                  headerStyle: {
+                     backgroundColor: Colors.primary,
+                  },
+                  headerTintColor: Colors.text,
+                  headerTitleStyle: {
+                     fontWeight: 'bold',
+                  },
+               }}>
+               <Stack.Screen
+                  name="Categories"
+                  component={CategoriesScreen}
+               />
+               <Stack.Screen
+                  name="Meals"
+                  component={CategoriesMealScreen}
+                  options={{
+                     title: 'Meals'
+                  }}
+               />
+               <Stack.Screen name="Details" component={MealDetailScreen} />
+            </Stack.Navigator>
+         </OverflowMenuProvider>
       </NavigationContainer>
    );
 }
