2fad64dd8542e2040c4327351c3801271dcfdd65
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-08-30 09:08:13 +0100

    ✨ Reusable meallist component added

---
+++ components/MealList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
+
+const MealList = ({ displayMeal, renderItems }) => {
+   return (
+      <View style={styles.container}>
+         <FlatList
+            data={displayMeal}
+            keyExtractor={(item) => item.id}
+            renderItem={renderItems}
+            style={{
+               width: '100%'
+            }}
+         />
+      </View>
+   )
+}
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+   },
+   title: {
+      fontSize: 20
+   }
+})
+
+export default MealList;
