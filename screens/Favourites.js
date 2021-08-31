76304987932afdecb8837bef7c385120b6b0040f
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-08-31 05:44:09 +0100

    ✨ backup text shown on favorite screen

--- screens/Favourites.js
+++ screens/Favourites.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import MealList from '../components/MealList';
 import { useSelector } from 'react-redux';
+import { Text, View } from 'react-native';
 
 const Favourite = ({ navigation }) => {
    const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+   if (favMeals.length === 0 || !favMeals) {
+      return (
+         <View style={{
+            flexDirection: 'row',
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center'
+         }}>
+            <Text style={{
+               fontSize: 20
+            }}>No Favorite meal!</Text>
+         </View>
+      )
+   }
+
    return <MealList data={favMeals} navigation={navigation} />
 }
 
